b7ecb4fe78ab6341a8e712d779b20fcec0f3e209
meitante <normyzh@163.com>
2025-12-24 19:14:44 +1300

    pp

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 var app = builder.Build();
 
 object HealthPayload() => new { status = "ok", service = "ecommerce-api" };
